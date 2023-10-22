bf350dc2a9b051201e5850b3b874d1c8416aab71
filip-h-999 <filip.hrenovcik@gmail.com>
2023-10-22 17:44:08 +0200

    turning pins off to reduce mA use

--- capTemp.py
+++ capTemp.py
@@ -3,10 +3,11 @@ import json
 import datetime
 import os
 from gpiozero import OutputDevice
+import time
 
 sensor = Adafruit_DHT.DHT11
-pinIn = 4
-pinOut = 11
+pinIn = 11
+pinOut = 4
 inside = "wetterDataInside.json"
 outside = "wetterDataOutside.json"
 
@@ -14,14 +15,17 @@ outside = "wetterDataOutside.json"
 relay_in = OutputDevice(pinIn, active_high=False, initial_value=False)
 relay_out = OutputDevice(pinOut, active_high=False, initial_value=False)
 
-def measure(file, pin, relay):
+powerPinIn = OutputDevice(10)
+powerPinOut = OutputDevice(2)
+
+def measure(file, pin, relay, powerPin):
     if os.path.exists(file):
         with open(file, "r") as f:
             data = json.load(f)
     else:
         data = {"allStats": []}
 
-    time = datetime.datetime.now()
+    current_time = datetime.datetime.now()
 
     allStats = data["allStats"]
     stats = {}
@@ -29,7 +33,7 @@ def measure(file, pin, relay):
     hum, temp = Adafruit_DHT.read_retry(sensor, pin)
     # print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp, hum))
 
-    stats["Time"] = time.strftime("%Y-%m-%d %H:%M:%S")
+    stats["Time"] = current_time.strftime("%Y-%m-%d %H:%M:%S")
     stats["Humidity"] = hum
     stats["Temp"] = temp
     allStats.append(stats)
@@ -38,10 +42,16 @@ def measure(file, pin, relay):
         json.dump({"allStats": allStats}, f, indent=4)
 
     # Turn off the relay after measurement
+    time.sleep(1)
     relay.off()
+    powerPin.off()
 
+powerPinIn.on()
 relay_in.on()  # Turn on the relay before measurement
-measure(inside, pinIn, relay_in)
+time.sleep(1)
+measure(inside, pinIn, relay_in, powerPinIn)
 
+powerPinOut.on()
 relay_out.on()  # Turn on the relay before measurement
-measure(outside, pinOut, relay_out)
+time.sleep(1)
+measure(outside, pinOut, relay_out, powerPinOut)
